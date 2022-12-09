749bcceadd4c8a12ae24d7d67bfb1c02cc076de1
Adna <aherak2@etf.unsa.ba>
2022-12-09 19:53:02 +0100

    Written PlaysDao

--- src/main/java/ba/unsa/etf/rpr/dao/PlaysDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/PlaysDao.java
@@ -1,9 +1,10 @@
 package ba.unsa.etf.rpr.dao;
+import ba.unsa.etf.rpr.domain.Artist;
 import ba.unsa.etf.rpr.domain.Directors;
 import ba.unsa.etf.rpr.domain.Plays;
 import ba.unsa.etf.rpr.domain.Writers;
 import java.util.List;
-
+import java.util.Date;
 
 /**
  * Dao interface for Plays domain bean
@@ -11,5 +12,11 @@ import java.util.List;
  */
 public interface PlaysDao extends Dao<Plays> {
 
+    List<Plays> searchByPrice(int prices);
+    List<Plays> searchByWriter(Writers writer);
+    List<Plays> searchByDirector(Directors director);
+    List<Plays> searchByArtist(Artist artist);
+    List<Plays> searchByDate(Date date);
+    List<Plays> searchByPlayName(String play_name);
 
 }
