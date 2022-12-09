7792d4786f4354c2dadea669eb392d7e8986ff7e
Adna <aherak2@etf.unsa.ba>
2022-12-09 19:05:01 +0100

    Implemented Writers domain

--- src/main/java/ba/unsa/etf/rpr/domain/Writers.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Writers.java
@@ -1,4 +1,50 @@
 package ba.unsa.etf.rpr.domain;
 
+import java.util.Objects;
+
 public class Writers {
+    private int writer_id;
+    private String first_name;
+    private String last_name;
+
+    public void setWriter_id(int writer_id) {
+        this.writer_id = writer_id;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public int getWriter_id() {
+        return writer_id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+    @Override
+    public String toString(){
+     return "Writer{ id="+writer_id+
+     ",first name="+first_name+
+     ",last name="+last_name;
+    }
+    @Override
+    public boolean equals(Object o){
+        if(this==o) return true;
+        if(o==null || o.getClass()!=getClass()) return false;
+        Writers w=(Writers) o;
+        return w.writer_id==writer_id;
+    }
+    @Override
+    public int hashCode(){
+        return Objects.hash(writer_id,first_name,last_name);
+    }
 }
