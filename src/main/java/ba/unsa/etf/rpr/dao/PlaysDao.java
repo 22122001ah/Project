4db1e73ed3deddda6b40bea54a5a14ed6f2cf34e
Adna <aherak2@etf.unsa.ba>
2022-12-09 20:05:16 +0100

    Added comments

--- src/main/java/ba/unsa/etf/rpr/dao/PlaysDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/PlaysDao.java
@@ -11,12 +11,53 @@ import java.util.Date;
  * @author Adna Herak
  */
 public interface PlaysDao extends Dao<Plays> {
-
+    /**
+     * searches Plays based on prices
+     * @param prices
+     * @return List of Plays with wanted price
+     */
     List<Plays> searchByPrice(int prices);
+
+    /**
+     * searches for Plays in given price range
+     * @param price1,price2
+     * @return List of Plays in given price range
+     */
+    List<Plays> searchByPrices(int price1,int price2);
+
+    /**
+     * searches for Plays written by wanted writer
+     * @param writer
+     * @return List of Plays written by wanted writer
+     */
     List<Plays> searchByWriter(Writers writer);
+
+    /**
+     * Searches for Plays directed by wanted director
+     * @param director
+     * @return List of Plays directed by wanted director
+     */
     List<Plays> searchByDirector(Directors director);
+
+    /**
+     * searches for Plays staring a certain Actor
+     * @param artist
+     * @return List of Plays staring a certain Actor
+     */
     List<Plays> searchByArtist(Artist artist);
+
+    /**
+     * searches for Plays that are playing on a certain date
+     * @param date
+     * @return List of Plays playing on a certain date
+     */
     List<Plays> searchByDate(Date date);
-    List<Plays> searchByPlayName(String play_name);
+
+    /**
+     * searches Plays by name
+     * @param play_name
+     * @return Play with that name
+     */
+    Plays searchByPlayName(String play_name);
 
 }
