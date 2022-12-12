96df3016de09c4854281a342a6c164414f9ddcb4
Adna <aherak2@etf.unsa.ba>
2022-12-12 15:14:14 +0100

    fxml file, MainFX and Controller

--- src/main/java/ba/unsa/etf/rpr/Controller.java
+++ src/main/java/ba/unsa/etf/rpr/Controller.java
@@ -1,4 +1,10 @@
 package ba.unsa.etf.rpr;
 
+import javafx.fxml.FXML;
+
 public class Controller {
+    @FXML
+    public void initialize() {
+
+    }
 }
--- src/main/java/ba/unsa/etf/rpr/MainFX.java
+++ src/main/java/ba/unsa/etf/rpr/MainFX.java
@@ -2,12 +2,22 @@ package ba.unsa.etf.rpr;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class MainFX extends Application {
-    @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader fl=new FXMLLoader(getClass().getResource(""));
-
+        FXMLLoader fl=new FXMLLoader(getClass().getResource("/fxml/home-layout.fxml"));
+        Controller controller = new Controller();
+        fl.setController(controller);
+        Parent root = null;
+        root = fl.load();
+        stage.setTitle("Hello, World!");
+        stage.setScene(new Scene(root, 300, 275));
+        stage.show();
+    }
+    public static void main(String[] args) {
+        launch(args);
     }
 }
