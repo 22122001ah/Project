e5c38aca76e4463001e82092d968adba6b0bcc9c
Adna <aherak2@etf.unsa.ba>
2022-12-18 23:27:50 +0100

    refactoring Dao

---
+++ src/main/java/ba/unsa/etf/rpr/dao/DaoFactory.java
@@ -0,0 +1,31 @@
+package ba.unsa.etf.rpr.dao;
+/**
+ * Factory method for singleton implementation of DAOs
+ *
+ * @author Adna Herak
+ */
+public class DaoFactory {
+    public static final PlaysDao playsDao=new PlaysDaoSQLimpl();
+    public static final plays_inDao playsin_Dao=new plays_inDaoSQLimpl();
+    public static final ArtistDao artistDao=new ArtistDaoSQLimpl();
+    public static final WritersDao writersDao=new WritersDaoSQLimpl();
+    public static final DirectorsDao directorsDao=new DirectorsDaoSQLimpl();
+
+    public DaoFactory() {
+    }
+    public static PlaysDao playsDao(){
+        return playsDao;
+    }
+    public static plays_inDao plays_inDao(){
+        return playsin_Dao;
+    }
+    public static ArtistDao artistDao(){
+        return artistDao;
+    }
+    public static WritersDao writersDao(){
+        return writersDao;
+    }
+    public static DirectorsDao directorsDao(){
+        return directorsDao;
+    }
+}
--- src/main/java/ba/unsa/etf/rpr/dao/plays_inDaoSQLimpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/plays_inDaoSQLimpl.java
@@ -1,4 +1,29 @@
 package ba.unsa.etf.rpr.dao;
 
-public class plays_inDaoSQLimpl {
+import ba.unsa.etf.rpr.domain.Artist;
+import ba.unsa.etf.rpr.domain.Plays;
+import ba.unsa.etf.rpr.domain.plays_in;
+
+import java.util.List;
+
+public class plays_inDaoSQLimpl implements plays_inDao{
+    @Override
+    public plays_in getById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<plays_in> getAll() {
+        return null;
+    }
+
+    @Override
+    public List<Plays> searchByArtist(Artist artist) {
+        return null;
+    }
+
+    @Override
+    public List<Artist> searchByPlay(Plays play) {
+        return null;
+    }
 }
