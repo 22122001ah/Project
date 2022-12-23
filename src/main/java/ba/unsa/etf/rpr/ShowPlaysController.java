dd0946f9117942ae51f84bb58d3954a48fd0ced3
Adna <aherak2@etf.unsa.ba>
2022-12-23 19:41:17 +0100

    new window(not finished)

---
+++ src/main/java/ba/unsa/etf/rpr/ShowPlaysController.java
@@ -0,0 +1,18 @@
+package ba.unsa.etf.rpr;
+import ba.unsa.etf.rpr.domain.Plays;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class ShowPlaysController {
+
+    @FXML
+    public void initialize() {
+
+    }
+
+
+}
