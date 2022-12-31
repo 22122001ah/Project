cab65c20ea244372ee61272b65f2e7b9aaebaf67
Adna <aherak2@etf.unsa.ba>
2022-12-31 13:24:51 +0100

    small changes

--- src/main/java/ba/unsa/etf/rpr/MainFX.java
+++ src/main/java/ba/unsa/etf/rpr/MainFX.java
@@ -6,7 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 public class MainFX extends Application {
     public void start(Stage stage) throws Exception {
         try{
@@ -18,7 +18,7 @@ public class MainFX extends Application {
             Parent root = null;
             root = fl.load();
             stage.setTitle("BuyTickets.com");
-            stage.setScene(new Scene(root, 600, 400));
+            stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
             stage.show();
         }
 
