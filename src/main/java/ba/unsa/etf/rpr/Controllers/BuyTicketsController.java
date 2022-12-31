33f47d2acad7935f241d6b57697b06ad4d079ec5
Adna <aherak2@etf.unsa.ba>
2022-12-31 11:12:46 +0100

    small changes-added date +price of single ticket

--- src/main/java/ba/unsa/etf/rpr/Controllers/BuyTicketsController.java
+++ src/main/java/ba/unsa/etf/rpr/Controllers/BuyTicketsController.java
@@ -1,5 +1,7 @@
 package ba.unsa.etf.rpr.Controllers;
 
+import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.exceptions.PlaysException;
 import javafx.beans.binding.IntegerBinding;
 import javafx.beans.property.ReadOnlyIntegerProperty;
 import javafx.collections.FXCollections;
@@ -15,16 +17,23 @@ public class BuyTicketsController {
     public Text price=new Text();
     public ChoiceBox<String> choiceBox=new ChoiceBox<>();
     public Label name=new Label();
+    public Label Price1=new Label();
+    public Label date=new Label();
     public void setName(String ime){
         name.setText(ime);
+        try {
+            date.setText(String.valueOf(DaoFactory.playsDao().searchByPlayName(ime).getDate()));
+        } catch (PlaysException e) {
+            throw new RuntimeException(e);
+        }
     }
     public void setPrice(int text) {
+        Price1.setText(String.valueOf(text));
         IntegerBinding Price= ReadOnlyIntegerProperty.readOnlyIntegerProperty(spinner.valueProperty()).multiply(text);
         price.textProperty().bind(Price.asString());
     }
     @FXML
     public void initialize(){
-
         String location[]={"Sarajevo","Banja Luka","Tuzla","Zenica","Mostar","Bihać","Trebinje","Livno"};
         choiceBox.setItems(FXCollections.observableArrayList(location));
         int initialValue = 1;
--- src/main/java/ba/unsa/etf/rpr/Controllers/ShowPlaysController.java
+++ src/main/java/ba/unsa/etf/rpr/Controllers/ShowPlaysController.java
@@ -1,5 +1,6 @@
 package ba.unsa.etf.rpr.Controllers;
 import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.exceptions.PlaysException;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -27,13 +28,12 @@ public class ShowPlaysController {
         return this.brojac;
     }
     public void PlayDesription(ActionEvent actionEvent) throws IOException {
-        try{        Button numberButton = (Button) actionEvent.getTarget();
-
+        try{     Button numberButton = (Button) actionEvent.getTarget();
             Stage Secondstage=new Stage();
             FXMLLoader fl=new FXMLLoader(getClass().getResource("/fxml/Info.fxml"));
             Parent root =fl.load();
             InfoController noviprozor=fl.getController();
-            noviprozor.setText(DaoFactory.playsDao().searchByPlayName(numberButton.getText()).getPlay_name());
+            noviprozor.setText(DaoFactory.playsDao().searchByPlayName(numberButton.getText()).toString());
             Secondstage.setTitle("Play description");
             Secondstage.setScene(new Scene(root,USE_COMPUTED_SIZE,USE_COMPUTED_SIZE));
             Secondstage.show();
@@ -43,6 +43,17 @@ public class ShowPlaysController {
             System.out.println(e);
         }
     }
-
+    public void buy(ActionEvent actionEvent) throws IOException, PlaysException {
+        Button numberButton = (Button) actionEvent.getTarget();
+        Stage secondstage=new Stage();
+        FXMLLoader fl=new FXMLLoader(getClass().getResource("/fxml/BuyTickets.fxml"));
+        Parent root=fl.load();
+        BuyTicketsController buyTicketsController=fl.getController();
+        buyTicketsController.setPrice(DaoFactory.playsDao().searchByPlayName(numberButton.getText()).getPrice());
+        buyTicketsController.setName(numberButton.getText());
+        secondstage.setTitle("Buy tickets");
+        secondstage.setScene(new Scene(root,USE_COMPUTED_SIZE,USE_COMPUTED_SIZE));
+        secondstage.show();
+    }
 
 }
